67b69c916095ee3a9968a0a0f287109a7a5019d9
Sam Ireland <mail@samireland.com>
2024-10-14 07:07:53 +0100

    Show tax returns

--- src/PersonalTaxYear.jsx
+++ src/PersonalTaxYear.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { data } from "./data";
-import { dateToTaxYear, formatCurrency } from "./utils";
+import { data, taxReturns, hmrcPayments } from "./data";
+import { dateToTaxYear, formatCurrency, formatDate } from "./utils";
 
 const PersonalTaxYear = props => {
 
@@ -39,52 +39,52 @@ const PersonalTaxYear = props => {
 
   const totalIncome = totalSalaryIncome + totalDividendIncome;
 
+  const taxReturn = taxReturns.find(t => t.taxYear === taxYear);
 
-  /* const transactions = allTransactions.filter(t => dateToTaxYear(t.date) === taxYear);
-  const components = transactions.filter(t => t.components).map(t => t.components.map(c => ({...c, company: t.company}))).flat();
-
-  const salaryComponents = components.filter(c => c.type === "salary");
-  const dividendComponents = components.filter(c => c.type === "dividend");
-
-  let totalSalaryIncome = 0;
-  const salaryIncomeByCompany = data.reduce((acc, c) => {
-    return {...acc, [c.name]: 0};
-  }, {});
-  for (const c of salaryComponents) {
-    const incomeTax = c.incomeTax || 0;
-    const employeeNI = c.employeeNI || 0;
-    const studentLoan = c.studentLoan || 0;
-    const grossIncome = c.amount + incomeTax + employeeNI + studentLoan;
-    totalSalaryIncome += grossIncome;
-    salaryIncomeByCompany[c.company] += grossIncome;
-  }
-
-  const totalDividendIncome = dividendComponents.reduce((acc, c) => acc + c.amount, 0);
-  const dividendIncomeByCompany = dividendComponents.reduce((acc, c) => {
-    return {...acc, [c.company]: (acc[c.company] || 0) + c.amount};
-  }, {});
-
-  const totalIncome = totalSalaryIncome + totalDividendIncome; */
-
+  const hmrcPaymentsForYear = hmrcPayments.filter(p => p.taxYear === taxYear);
+  const hasPayments = hmrcPaymentsForYear.length > 0;
 
   return (
     <div>
       <h2 className="text-3xl font-medium">{taxYear}/{taxYear+1}</h2>
-      <div className="text-xl">Total income: {formatCurrency(totalIncome)}</div>
-      <div className="ml-8 text-lg">Total salary income: {formatCurrency(totalSalaryIncome)}</div>
-      {data.map(c => (
-        <div key={c.name} className="ml-16">
-          <span className="size-3 rounded-full inline-block mr-1" style={{backgroundColor: c.color}} />
-          {c.name}: {formatCurrency(salaryIncomeByCompany[c.name])}
+
+      <div className="flex gap-x-6">
+        <div>
+          <div className="text-xl">Total income: {formatCurrency(totalIncome)}</div>
+          <div className="ml-8 text-lg">Total salary income: {formatCurrency(totalSalaryIncome)}</div>
+          {data.map(c => (
+            <div key={c.name} className="ml-16">
+              <span className="size-3 rounded-full inline-block mr-1" style={{backgroundColor: c.color}} />
+              {c.name}: {formatCurrency(salaryIncomeByCompany[c.name])}
+            </div>
+          ))}
+          <div className="ml-8 text-lg">Total dividend income: {formatCurrency(totalDividendIncome)}</div>
+          {data.map(c => (
+            <div key={c.name} className="ml-16">
+              <span className="size-3 rounded-full inline-block mr-1" style={{backgroundColor: c.color}} />
+              {c.name}: {formatCurrency(dividendIncomeByCompany[c.name] || 0)}
+            </div>
+          ))}
         </div>
-      ))}
-      <div className="ml-8 text-lg">Total dividend income: {formatCurrency(totalDividendIncome)}</div>
-      {data.map(c => (
-        <div key={c.name} className="ml-16">
-          <span className="size-3 rounded-full inline-block mr-1" style={{backgroundColor: c.color}} />
-          {c.name}: {formatCurrency(dividendIncomeByCompany[c.name] || 0)}
-        </div>
-      ))}
+        {taxReturn && (
+          <div>
+            <div className="text-lg">HMRC Owed: {formatCurrency(taxReturn.incomeTax + taxReturn.studentLoan)}</div>
+            <div className="text-base ml-8">Income tax: {formatCurrency(taxReturn.incomeTax)}</div>
+            <div className="text-base ml-8">Student loan: {formatCurrency(taxReturn.studentLoan)}</div>
+            {hasPayments && (
+              <>
+                <div className="text-lg">HMRC Payments: {formatCurrency(hmrcPaymentsForYear.reduce((acc, p) => acc + p.amount, 0))}</div>
+                {hmrcPaymentsForYear.map(p => (
+                  <div key={p.date} className="text-base ml-8">
+                    {formatDate(p.date)}: {formatCurrency(p.amount)}
+                  </div>
+                ))}
+              </>
+            )}
+            <div className="text-lg">Oustanding: {formatCurrency(taxReturn.incomeTax + taxReturn.studentLoan - hmrcPaymentsForYear.reduce((acc, p) => acc + p.amount, 0))}</div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,7 @@ export const dateToTaxYear = (date, monthStart=4) => {
 export const formatCurrency = amount => {
   return amount.toLocaleString("en-GB", { style: "currency", currency: "GBP" });
 }
+
+export const formatDate = date => {
+  return new Date(date).toLocaleDateString("en-GB");
+}
