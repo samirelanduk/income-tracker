789af747814e5a557acc53a65bbc4d7b88f252ac
Sam Ireland <mail@samireland.com>
2024-10-15 08:32:30 +0100

    Lock nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ModeSwitcher from "./ModeSwitcher";
 import PersonalFinances from "./PersonalFinances";
 import CompanyFinances from "./CompanyFinances";
 import FutureToggle from "./FutureToggle";
+import { dateToTaxYear } from "./utils";
 
 const App = () => {
 
@@ -12,21 +13,15 @@ const App = () => {
 
   const allTransactions = data.flatMap(c => c.transactions);
 
-  const dateToTaxYear = date => {
-    const year = parseInt(date.split("-")[0]);
-    const month = parseInt(date.split("-")[1]);
-    return month < 4 ? year - 1 : year;
-  }
-
   const taxYears = [...new Set(allTransactions.map(t => dateToTaxYear(t.date)))];
   taxYears.sort((a, b) => b - a);
 
   return (
-    <div className="bg-gray-100  pl-14 pr-10 pt-6 pb-12 text-gray-700">
-      <div className="flex justify-between items-center mb-12">
+    <div className="bg-gray-100 pl-14 pr-10 pt-20 pb-12 text-gray-700">
+      <nav className="flex justify-between items-center mb-12 fixed top-0 left-0 pl-14 right-0 pr-12 h-14 bg-gray-100 z-30">
         <FutureToggle useFuture={useFuture} setUseFuture={setUseFuture} />
         <ModeSwitcher mode={mode} setMode={setMode} />
-      </div>
+      </nav>
       {mode === "Personal" && <PersonalFinances useFuture={useFuture} />}
       {mode !== "Personal" && <CompanyFinances name={mode} />}
     </div>
